4f21e89e03e163a2f8ac1ff6418a7980838a667f
Alexsander Campos <ext-alexsander.boas@bbts.com.br>
2026-01-31 06:13:58 -0300

    adding cursos module

---
+++ src/app/modules/cursos-list/cursos-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './cursos-list.component.html',
+  styleUrl: './cursos-list.component.css'
+})
+export class CursosListComponent {
+
+}
